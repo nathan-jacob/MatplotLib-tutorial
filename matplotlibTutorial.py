c29ae273569a047e20a20f1b47124a4bed5801bf
Nathan Jacob <37455968+nathan-jacob@users.noreply.github.com>
2019-08-17 12:41:37 -0500

    Made Linear Graph into Stack Chart

--- matplotlibTutorial.py
+++ matplotlibTutorial.py
@@ -1,20 +1,22 @@
 import matplotlib.pyplot as plt
 
-x = [1,2,3]
-y = [5,6,7]
+days = [1,2,3,4,5]
 
-x2 = [1,2,3]
-y2 = [10,14,12]
-
-
-plt.plot(x,y, label="First Line")
-plt.plot(x2,y2, Label = "Second Line")
-
-#add labels
-plt.xlabel("Plot Number")
-plt.ylabel("Important Var")
+sleeping = [7,8,6,11,7]
+eating =   [2,3,4,3,2]
+working =  [7,8,7,2,2]
+playing =  [8,5,7,8,13]
 
 
-plt.title("Interesting Graph")
+plt.plot([],[],color='m', label='Sleeping', linewidth=5)
+plt.plot([],[],color='c', label='Eating', linewidth=5)
+plt.plot([],[],color='r', label='Working', linewidth=5)
+plt.plot([],[],color='k', label='Playing', linewidth=5)
+
+plt.stackplot(days, sleeping,eating,working,playing, colors=['m','c','r','k'])
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
 plt.legend()
 plt.show()
